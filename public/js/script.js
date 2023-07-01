3c5ea89e251e6a122616fdaf6fdcf5852d7a28d0
Pranjal Nikhare <91018218+pranjal-nikhare@users.noreply.github.com>
2023-07-01 11:22:18 +0530

    Add files via upload

---
+++ public/js/script.js
@@ -0,0 +1,8 @@
+placeSearch({
+  key: "SN9nFjkyON4sAUt3G1ONpUIHTnGJ5j0K",
+  container: document.querySelector("#place-search-input "),
+});
+placeSearch({
+  key: "SN9nFjkyON4sAUt3G1ONpUIHTnGJ5j0K",
+  container: document.querySelector("#place-search-input2 "),
+});
