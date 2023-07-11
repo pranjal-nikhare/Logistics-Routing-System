fe1da5fe4a8f053a73f84911cf8cf6f770847637
Pranjal Nikhare <91018218+pranjal-nikhare@users.noreply.github.com>
2023-07-11 17:07:27 +0530

    Update database.js

--- src/database.js
+++ src/database.js
@@ -1,110 +1,82 @@
-// import mysql from "mysql2";
-// import dotenv from "dotenv";
-
-// dotenv.config();
-
-// // // const pool = mysql
-// // //    .createPool({
-// // //     host: "localhost",
-// // //     user: "root",
-// // //     password: "pranjal123",
-// // //     database: "Delivery_Data",
-// // //   })
-// // //   .promise();
-
-// const pool = mysql
-//   .createPool({
-//     host: process.env.MYSQL_HOST,
-//     user: process.env.MYSQL_USER,
-//     password: process.env.MYSQL_PASSWORD,
-//     database: process.env.MYSQL_DATABASE,
-//   })
-//   .promise();
-
-// async function getBasicData() {
-//   const [rows] = await pool.query("SELECT * FROM BasicData");
-//   return rows;
-// }
-
-// async function getBasicDataById(id) {
-//   const [rows] = await pool.query("SELECT * FROM BasicData WHERE id=?", [id]);
-//   return rows[0];
-// }
-
-// async function addRequest(Name, Origin, Destination) {
-//   const [result] = await pool.query(
-//     "INSERT INTO BasicData (Name, Origin, Destination) VALUES (?, ?, ?)",
-//     [Name, Origin, Destination]
-//   );
-//   return {
-//     id: result.insertId,
-//     Name,
-//     Origin,
-//     Destination,
-//   };
-// }
-
-// const result = await addRequest("test", "test", "test");
-// console.log(result);
-
 const mysql = require("mysql2");
 const dotenv = require("dotenv");
-
 dotenv.config();
 
-const pool = mysql
-  .createPool({
-    host: process.env.MYSQL_HOST,
-    user: process.env.MYSQL_USER,
-    password: process.env.MYSQL_PASSWORD,
-    database: process.env.MYSQL_DATABASE,
-  })
-  .promise();
+const pool = mysql.createPool({
+  host: process.env.MYSQL_HOST,
+  user: process.env.MYSQL_USER,
+  password: process.env.MYSQL_PASSWORD,
+  database: process.env.MYSQL_DATABASE,
+});
 
-async function getBasicData() {
-  const [rows] = await pool.query("SELECT * FROM BasicData");
-  return rows;
+function insertData(
+  Name,
+  Origin,
+  Origin_coordinates,
+  Destination,
+  Destination_coordinates
+) {
+  return new Promise((resolve, reject) => {
+    const sql =
+      "INSERT INTO BasicData (Name, Origin, Origin_coordinates, Destination, Destination_coordinates) VALUES (?, ?, ST_GeomFromText(?), ?, ST_GeomFromText(?))";
+    const values = [
+      Name,
+      Origin,
+      `POINT(${Origin_coordinates[0]} ${Origin_coordinates[1]})`,
+      Destination,
+      `POINT(${Destination_coordinates[0]} ${Destination_coordinates[1]})`,
+    ];
+
+    pool.query(sql, values, (err, result) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(result);
+      }
+    });
+  });
 }
 
-async function getBasicDataById(id) {
-  const [rows] = await pool.query("SELECT * FROM BasicData WHERE id=?", [id]);
-  return rows[0];
+function getDataById(id) {
+  return new Promise((resolve, reject) => {
+    pool.query("SELECT * FROM BasicData WHERE id=?", [id], (err, result) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(result);
+      }
+    });
+  });
 }
 
-async function addRequest(Name, Origin, Destination) {
-  const [result] = await pool.query(
-    "INSERT INTO BasicData (Name, Origin, Destination) VALUES (?, ?, ?)",
-    [Name, Origin, Destination]
-  );
-  return {
-    id: result.insertId,
-    Name,
-    Origin,
-    Destination,
-  };
+function getLastID() {
+  return new Promise((resolve, reject) => {
+    pool.query(
+      "SELECT * FROM BasicData ORDER BY id DESC LIMIT 1",
+      (err, result) => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve(result);
+        }
+      }
+    );
+  });
 }
 
-const result = addRequest("Mumbai", "Chennai", "12/12/2021");
-console.log(result);
-
-async function main() {
-  try {
-    const result = await addRequest("?", "?", "?");
-    console.log(result);
-  } catch (error) {
-    console.error(error);
-  } finally {
-    pool.end(); // Close the database connection when done
-  }
+function getCoordinates() {
+  return new Promise((resolve, reject) => {
+    pool.query(
+      "SELECT ST_AsText(Origin_coordinates) AS origin_wkt, ST_AsText(Destination_coordinates) AS destination_wkt FROM BasicData;",
+      (err, result) => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve(result);
+        }
+      }
+    );
+  });
 }
-// async function establishConnection() {
-//   try {
-//     await pool.query("SELECT 1"); // Execute a simple query to test the connection
-//     console.log("Database connection established successfully!");
-//   } catch (error) {
-//     console.error("Error establishing database connection:", error);
-//     process.exit(1); // Terminate the application if the connection fails
-//   }
-// }
 
-// main();
+module.exports = { insertData, getDataById, getLastID, getCoordinates };
